4511b854ecd04eb211976f4ee7648006491d010f
Akande Benji <adeoul001@gmail.com>
2021-05-16 12:07:35 -0700

    Created a Success Page

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -5,8 +5,14 @@ const viewRouter = express.Router()
 const viewController = require('../controllers/viewController')
 const bookingController = require('../controllers/bookingController')
 
-viewRouter.route('/').get(bookingController.createBookingCheckout,
-    
+viewRouter.route("/successfulPayment").get((req,res,next)=>{
+    res.status(200).render('successPage',{
+      data:"An email would be sent to you shortly"
+    })
+  })
+viewRouter.route('/')
+.get(
+    // bookingController.createBookingCheckout,
     viewController.getOverview)
 viewRouter.route('/ticket/:slug').get(viewController.getTicketDetail)
 
